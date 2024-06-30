bcb2b069644cda42d1cb05344cd61bc929758199
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-06-30 20:38:06 +0300

    feat: change some styles

--- app.vue
+++ app.vue
@@ -2,8 +2,19 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
-    <div class="m-8 bg-amber-50">
-      1234
+    <div
+        class="m-8 bg-amber-50"
+    >
+      <div>
+        div
+      </div>
+      <button
+          class="hover:bg-blue-500 transition pt-1 pb-1 pl-2 pr-2 rounded-md bg-blue-600 text-white"
+      >
+        button
+      </button>
     </div>
   </div>
 </template>
+<script setup lang="ts">
+</script>
